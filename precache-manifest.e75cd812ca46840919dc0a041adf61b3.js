4d531917a849ea958986e767c40266454a0d8521
ravikanojiya <ravikanojiya45@gmail.com>
2020-10-11 14:38:37 +0530

    Updates

---
+++ precache-manifest.e75cd812ca46840919dc0a041adf61b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2debf6af0ac0b6d631b1bd1e9981e905",
+    "url": "/react-covid19/index.html"
+  },
+  {
+    "revision": "815ab57e4d0bc65946a7",
+    "url": "/react-covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9d2fb05b4d9f039752b",
+    "url": "/react-covid19/static/js/2.db928525.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-covid19/static/js/2.db928525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815ab57e4d0bc65946a7",
+    "url": "/react-covid19/static/js/main.c2031082.chunk.js"
+  },
+  {
+    "revision": "6e6bb6e8becb51a5582d",
+    "url": "/react-covid19/static/js/runtime-main.2bd54a1d.js"
+  }
+]);
